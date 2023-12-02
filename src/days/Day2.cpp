215e0558abb2f029fe16ae767b29bb386189200e
nikitadpopel <nikita.d.popel@gmail.com>
2023-12-02 08:51:10 -0500

    added day2, will clean up

--- src/days/Day2.cpp
+++ src/days/Day2.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <algorithm>
 #include <unordered_map>
+#include <array>
 
 #include "Day2.h"
 #include "../helpers/Reader.h"
@@ -13,12 +14,140 @@ Day2::Day2()
 
 int Day2::Part1()
 {
-	return 0;
+	int s_red = 12;
+	int s_green = 13;
+	int s_blue = 14;
+	int id_sum = 0;
+	std::vector<std::string>::iterator iter;
+	for(iter = m_vecInput.begin(); iter < m_vecInput.end(); iter++ )
+	{
+		if(!iter->empty())
+		{
+			int curr_id = stoi(iter->substr(iter->find(" "), iter->find(":")));
+			std::string rounds_str = iter->substr(iter->find(":") + 2, -1);
+			std::vector<std::string> rounds;
+			while(!rounds_str.empty())
+			{
+				auto delim_i = rounds_str.find(",");
+				auto sc_i = rounds_str.find(";");
+				if (delim_i != std::string::npos)
+				{
+					if(sc_i != std::string::npos && sc_i < delim_i)
+					{
+						delim_i = sc_i;
+					}
+				}
+				else if (sc_i != std::string::npos)
+				{
+					delim_i = sc_i;
+				}
+				if(delim_i != std::string::npos)
+				{
+					rounds.push_back(rounds_str.substr(0, delim_i));
+					rounds_str = rounds_str.substr(delim_i + 2, -1);
+				}
+				else
+				{
+					rounds.push_back(rounds_str);
+					rounds_str = "";
+				}
+				
+			}
+			bool is_possible = true;
+			for (auto & element : rounds)
+			{
+				int val = stoi(element.substr(0, element.find(" ")));
+				if(element.find("red") != std::string::npos && val > s_red)
+				{
+					is_possible = false;
+				}
+				if(element.find("green") != std::string::npos && val > s_green)
+				{
+					is_possible = false;
+				}
+				if(element.find("blue") != std::string::npos && val > s_blue)
+				{
+					is_possible = false;
+				}
+			}
+			if(is_possible)
+			{
+				id_sum += curr_id;
+			}
+		}
+	}
+
+
+	return id_sum;
 }
 
 int Day2::Part2()
 {
-	return 0;
+	int s_red = 12;
+	int s_green = 13;
+	int s_blue = 14;
+	int id_sum = 0;
+	std::vector<std::string>::iterator iter;
+	for(iter = m_vecInput.begin(); iter < m_vecInput.end(); iter++ )
+	{
+		if(!iter->empty())
+		{
+			int curr_id = stoi(iter->substr(iter->find(" "), iter->find(":")));
+			std::string rounds_str = iter->substr(iter->find(":") + 2, -1);
+			std::vector<std::string> rounds;
+			while(!rounds_str.empty())
+			{
+				auto delim_i = rounds_str.find(",");
+				auto sc_i = rounds_str.find(";");
+				if (delim_i != std::string::npos)
+				{
+					if(sc_i != std::string::npos && sc_i < delim_i)
+					{
+						delim_i = sc_i;
+					}
+				}
+				else if (sc_i != std::string::npos)
+				{
+					delim_i = sc_i;
+				}
+				if(delim_i != std::string::npos)
+				{
+					rounds.push_back(rounds_str.substr(0, delim_i));
+					rounds_str = rounds_str.substr(delim_i + 2, -1);
+				}
+				else
+				{
+					rounds.push_back(rounds_str);
+					rounds_str = "";
+				}
+				
+			}
+			int max_red = 0;
+			int max_green = 0;
+			int max_blue = 0;
+			for (auto & element : rounds)
+			{
+				int val = stoi(element.substr(0, element.find(" ")));
+				if(element.find("red") != std::string::npos && val > max_red)
+				{
+					max_red = val;
+				}
+				if(element.find("green") != std::string::npos && val > max_green)
+				{
+					max_green = val;
+				}
+				if(element.find("blue") != std::string::npos && val > max_blue)
+				{
+					max_blue = val;
+				}
+			}
+			id_sum += max_red * max_green * max_blue;
+
+		}
+	}
+
+
+	return id_sum;
 }
 
 bool Day2::Solve()
