41103932e62e5ef410a5add0756d8c192fbbabe9
nikitadpopel <nikita.d.popel@gmail.com>
2023-12-02 21:46:40 -0500

    added prep for day3

---
+++ src/days/Day3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <array>
+
+#include "Day3.h"
+#include "../helpers/Reader.h"
+
+Day3::Day3()
+{
+	m_vecInput = ReadInput("C:\\Users\\mpopel\\Documents\\github\\advent_of_code_2023\\src\\inputs\\day3.input");
+}
+
+int Day3::Part1()
+{
+	return 0;
+}
+
+int Day3::Part2()
+{
+	return 00;
+}
+
+bool Day3::Solve()
+{
+	std::cout << Part1() <<std::endl;
+	std::cout << Part2() <<std::endl;
+
+	return true;
+}
---
+++ src/days/Day3.h
@@ -0,0 +1,13 @@
+#include <vector>
+#include <string>
+
+class Day3
+{
+    public:
+        Day3();
+        int Part1();
+        int Part2();
+        bool Solve();
+    private:
+        std::vector<std::string> m_vecInput;
+};
